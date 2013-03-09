2eddfd8c559ae629b98fde2deb33c6ef05503432
Frederick Jansen <frederick.jansen@gmail.com>
2013-03-09 16:35:53 +0100

    Added list comparison files

---
+++ src/be/alfredo/colruyt/ComparisonListActivity.java
@@ -0,0 +1,20 @@
+package be.alfredo.colruyt;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alfredo
+ * Date: 09/03/13
+ * Time: 16:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComparisonListActivity extends ListActivity
+{
+    @Override
+    public void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+    }
+}
